a91b43a59aeedff2ee865b2dc3cb94cb28fddabd
Paweł Olkiewicz <olkpaw@gmail.com>
2022-01-11 19:42:56 +0100

    Version 0.0.2

--- gui_main.py
+++ gui_main.py
@@ -1,31 +1,60 @@
 import sys
+import threading
 
-from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import QStatusBar, QApplication, QMainWindow
+import PyQt5
+from PyQt5 import QtGui
+from PyQt5.QtCore import QObject, pyqtSlot, pyqtSignal, QPoint, QEvent, QRect, QMetaObject, QCoreApplication, QSize
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtWidgets import QStatusBar, QApplication, QMainWindow, QWidget, QGridLayout, QFrame, QLCDNumber, \
+    QPushButton, QMenuBar
+
+import time
 
 
-from PySide2.QtCore import *
-from PySide2.QtGui import *
-from PySide2.QtWidgets import *
+class MouseObserver(PyQt5.QtCore.QObject):
+    pressed = pyqtSignal(QPoint, QPoint)
+    released = pyqtSignal(QPoint, QPoint)
+    moved = pyqtSignal(QPoint, QPoint)
+
+    def __init__(self, window):
+        super().__init__(window)
+        self._window = window
+
+        self.window.installEventFilter(self)
+
+    @property
+    def window(self):
+        return self._window
+
+    def eventFilter(self, obj, event):
+        if self.window is obj:
+            if event.type() == QEvent.MouseButtonPress:
+                self.pressed.emit(event.pos(), event.globalPos())
+            elif event.type() == QEvent.MouseMove:
+                self.moved.emit(event.pos(), event.globalPos())
+            elif event.type() == QEvent.MouseButtonRelease:
+                self.released.emit(event.pos(), event.globalPos())
+        return super().eventFilter(obj, event)
 
 
-class UiMainWindow(object):
+class UiMainWindow(QMainWindow):
 
-    def __init__(self, main_window):
-        if not main_window.objectName():
-            main_window.setObjectName(u"MainWindow")
+    def __init__(self):
+        super().__init__()
+        if not self.objectName():
+            self.setObjectName(u"MainWindow")
 
-        main_window.resize(800, 800)
-        self.central_widget = QWidget(main_window)
+        self.resize(800, 800)
+        self.central_widget = QWidget(self)
         self.central_widget.setObjectName(u"centralwidget")
         self.gridLayoutWidget = QWidget(self.central_widget)
         self.gridLayoutWidget.setObjectName(u"gridLayoutWidget")
-        self.gridLayoutWidget.setGeometry(QRect(100, 120, 605, 605))
+        self.gridLayoutWidget.setGeometry(QRect(100, 100, 600, 600))
         self.gridLayout = QGridLayout(self.gridLayoutWidget)
         self.gridLayout.setObjectName(u"gridLayout")
         self.gridLayout.setContentsMargins(0, 0, 0, 0)
-        self.gridLayout.setSpacing(1)
-        self.gridLayout.setGeometry(QRect(0, 0, 605, 605))
+        self.gridLayout.setSpacing(0)
+        self.gridLayout.setGeometry(QRect(0, 0, 600, 600))
         self.frame = QFrame(self.gridLayoutWidget)
         self.frame.setObjectName(u"frame")
         self.frame.setFrameShape(QFrame.StyledPanel)
@@ -42,32 +71,38 @@ class UiMainWindow(object):
         self.pushButton = QPushButton(self.central_widget)
         self.pushButton.setObjectName(u"pushButton")
         self.pushButton.setGeometry(QRect(350, 50, 111, 41))
-        main_window.setCentralWidget(self.central_widget)
-        self.menubar = QMenuBar(main_window)
+        self.setCentralWidget(self.central_widget)
+        self.menubar = QMenuBar(self)
         self.menubar.setObjectName(u"menubar")
         self.menubar.setGeometry(QRect(0, 0, 843, 21))
-        main_window.setMenuBar(self.menubar)
-        self.statusbar = QStatusBar(main_window)
+        self.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(self)
         self.statusbar.setObjectName(u"statusbar")
-        main_window.setStatusBar(self.statusbar)
+        self.setStatusBar(self.statusbar)
 
-        self.retranslateUi(main_window)
+        self.mine_icon = QIcon()
+        self.mine_icon.addPixmap(QPixmap('mine_icon.png'))
 
-        QMetaObject.connectSlotsByName(main_window)
+        self.retranslateUi(self)
+
+        QMetaObject.connectSlotsByName(self)
 
         self.mines = []
         i = 0
         for x in range(6):
             row = []
             for y in range(6):
-                push_button = QPushButton(f'[{x}][{y}]')
+                push_button = QPushButton()
+                push_button.setObjectName(f'[{x}][{y}]')
                 push_button.setFixedSize(100, 100)
-                push_button.clicked.connect(lambda z=x, t=y: self.on_click(z, t))
-                # QObject.connect(push_button, self.on_click)
+                push_button.clicked.connect(lambda state, z=x, t=y: self.on_click(z, t))
                 self.gridLayout.addWidget(push_button, x, y)
                 row.append(push_button)
                 i += 1
             self.mines.append(row)
+
+        self.time_start = -1
+        self.timer_stop = False
     # setupUi
 
     def retranslateUi(self, MainWindow):
@@ -75,14 +110,44 @@ class UiMainWindow(object):
         self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Start / Reset", None))
     # retranslateUi
 
-    @pyqtSlot()
+    def in_thread(self):
+        while not self.timer_stop and self.time_start < 1000:
+            time.sleep(1)
+            self.time_start += 1
+            self.time.display(self.time_start)
+
     def on_click(self, a, b):
+        self.mines[a][b].setStyleSheet("background-color : #bfbfbf")
+        self.mines[a][b].setText('')
+        self.mines[a][b].setIcon(self.mine_icon)
+        self.mines[a][b].setIconSize(QSize(30, 30))
         print(a, b)
+
+        if self.time_start == -1:
+            self.time_start = 0
+            self.thread = threading.Thread(target=self.in_thread)
+            self.thread.start()
+
+    def handle_window_released(self, window_pos: QPoint, global_pos: QPoint):
+        x = (window_pos.x() - 100) // 100
+        y = (window_pos.y() - 100) // 100
+        print(f'handle_window_released: {x}, {y}')
+
+        if self.time_start == -1:
+            self.time_start = 0
+            self.thread = threading.Thread(target=self.in_thread)
+            self.thread.start()
+
+    def closeEvent(self, a0: QtGui.QCloseEvent):
+        self.timer_stop = True
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    my_win = UiMainWindow(win)
-    win.show()
+    my_win = UiMainWindow()
+    my_win.show()
+
+    mouse_observer = MouseObserver(my_win)
+    mouse_observer.released.connect(my_win.handle_window_released)
+
     sys.exit(app.exec_())
---
+++ minesweeper_graphic.py
@@ -0,0 +1,140 @@
+import math
+import random
+from enum import Enum
+
+import numpy as np
+
+
+class Action(Enum):
+    CHECK_AS_MINE = 1
+    DISCOVER = 2
+
+
+class Minesweeper:
+    def __init__(self, size: int):
+        self.game_over = False
+        self.size = size
+        self.board = np.zeros((size, size), dtype=int)
+        #  0 - nothing, not discovered; -91 - mine;
+        # -92 - checked as mine;        -93 nothing, discovered
+        self.checked_as_mine = 0
+        self.number_of_mines = 5
+        self.list_of_mines = []
+
+    def add_mines(self, x: int, y: int):
+        _number_of_mines = 0
+
+        while True:
+            if _number_of_mines == self.number_of_mines:
+                break
+
+            mine_x = random.randint(0, self.size - 1)
+            mine_y = random.randint(0, self.size - 1)
+
+            if math.dist((x, y), (mine_x, mine_y)) <= 3.0 or (mine_x, mine_y) in self.list_of_mines:
+                continue
+
+            self.board[mine_x, mine_y] = -91
+            self.list_of_mines.append((mine_x, mine_y))
+            _number_of_mines += 1
+
+    def recalculate_board(self):
+        for x in range(self.size):
+            for y in range(self.size):
+                if self.board[x, y] == -91:
+                    continue
+
+                mines_around = 0
+                x_min = x - 1 if x > 0 else x
+                x_max = x + 1 if x + 1 < self.size else x
+                y_min = y - 1 if y > 0 else y
+                y_max = y + 1 if y + 1 < self.size else y
+
+                for x_around in range(x_min, x_max + 1):
+                    for y_around in range(y_min, y_max + 1):
+                        if x_around == x and y_around == y:
+                            continue
+
+                        if self.board[x_around, y_around] == -91:
+                            mines_around += 1
+
+                self.board[x, y] = mines_around + 10 if mines_around > 0 else mines_around
+
+    def first_action(self, x: int, y: int):
+        self.add_mines(x, y)
+        self.recalculate_board()
+        self.discover(x, y)
+
+    def action(self, x: int, y: int, action: Action):
+        value = self.board[x, y]
+        if value == -93:
+            return False
+
+        if action == Action.DISCOVER:
+            if value == -91:
+                self.game_over = True
+            # elif value == -93:
+            else:
+                self.discover(x, y)
+
+        elif action == Action.CHECK_AS_MINE:
+            if 1 <= value <= 9:
+                return
+
+            if value == -92:
+                if (x, y) in self.list_of_mines:
+                    self.board[x, y] = -91
+                else:
+                    self.board[x, y] = 0
+                return
+
+            if self.checked_as_mine >= self.size:
+                return False
+
+            self.board[x, y] = -92
+            self.checked_as_mine += 1
+
+            if self.checked_as_mine == self.number_of_mines and not np.any(self.board == 0) and not np.any(self.board == -91):
+                # TODO: maybe we should check if all fields checked as mines are in list self.list_of_mines
+                self.game_over = True
+
+    def discover(self, x: int, y: int):
+        # FIXME: algorithm has some bugs, but it works
+        value = self.board[x, y]
+        if value == -91:
+            self.game_over = True
+            return False
+
+        if value not in [0, -93, 11, 12, 13, 14, 15, 16, 17, 18, 19]:
+            return
+
+        if 10 < value <= 19:
+            self.board[x, y] -= 10
+            return
+
+        x_min = x - 1 if x > 0 else x
+        x_max = x + 1 if x + 1 < self.size else x
+        y_min = y - 1 if y > 0 else y
+        y_max = y + 1 if y + 1 < self.size else y
+
+        for x_around in range(x_min, x_max + 1):
+            for y_around in range(y_min, y_max + 1):
+                if x_around == x and y_around == y:
+                    continue
+
+                around_value = self.board[x_around, y_around]
+                if around_value == 0:
+                    self.board[x_around, y_around] = -93
+                    self.discover(x_around, y_around)
+                elif around_value > 10:
+                    self.board[x_around, y_around] -= 10
+                    break
+
+        self.board[x, y] = -93
+
+    def __str__(self):
+        return self.board.__str__() + '\n\n'  + self.board.__str__().replace('0', '-').replace('-93', '   ').replace('-91', '  -').replace('11', ' -').replace('12', ' -').replace('13', ' -').replace('14', ' -').replace('15', ' -').replace('-92', '  T')
+        # return self.board.__str__().replace('0', '-').replace('-93', '   ').replace('-91', '   ').replace('11', '  ').replace('12', '  ').replace('13', '  ').replace('14', '  ')
+
+    def __repr__(self):
+        return self.board.__str__()
